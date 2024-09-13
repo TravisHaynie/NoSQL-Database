34f374af5820c8ed25806f596db610d745ee967a
unknown <travihay@gmail.com>
2024-09-13 18:57:03 -0500

    added code for models and userroutes along with usercontrollers

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+// Schema for the User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must match a valid email address!'],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Virtual to retrieve the length of the user's friends array
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+// Initialize User model
+const User = model('User', userSchema);
+
+module.exports = User;
